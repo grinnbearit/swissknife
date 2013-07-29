eeb39f18a5cfed6076e8a07fd92377f9ef760962
Sidhant Godiwala <grinnbearit@liara.local>
2013-07-29 13:11:55 +0530

    queue

--- src/util/core.clj
+++ src/util/core.clj
@@ -5,3 +5,9 @@
   "Returns the last n elements of the sequence"
   [n xs]
   (drop (- (count xs) n) xs))
+
+
+(defn queue
+  "Returns a clojure.lang.PersistentQueue initialized with xs"
+  [& xs]
+  (reduce conj (clojure.lang.PersistentQueue/EMPTY) xs))
--- test/util/core_test.clj
+++ test/util/core_test.clj
@@ -9,3 +9,15 @@
 
  (keep-last 3 [0 1])
  => [0 1])
+
+
+(facts
+ (queue) => []
+
+ (queue 1 2 3) => [1 2 3]
+
+ (peek (queue 1 2 3)) => 1
+
+ (pop (queue 1 2 3)) => [2 3]
+
+ (conj (queue 1 2 3) 4) => [1 2 3 4])
