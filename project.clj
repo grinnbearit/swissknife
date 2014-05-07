c1bfc1815b96fc91bc3d3b46e905ed0e394f8cae
Sidhant Godiwala <grinnbearit@liara.local>
2014-05-07 17:57:35 +0530

    bump libs

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "general purpose utility functions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:plugins [[lein-midje "3.0.1"]]
-                   :dependencies [[midje "1.5.1"]]}})
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
+                   :dependencies [[midje "1.6.3"]]}})
