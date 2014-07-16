d9967266eb613a01cc3a6cb3402afc131ec4d4ee
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2014-07-16 12:55:22 +0530

    group

--- src/clojure/swissknife/core.clj
+++ src/clojure/swissknife/core.clj
@@ -1,7 +1,26 @@
-(ns swissknife.core)
+(ns swissknife.core
+  (:use [clojure.walk :only [walk]]))
 
 
 (defn keep-last
   "Returns the last n elements of the sequence"
   [n xs]
   (drop (- (count xs) n) xs))
+
+
+(defn group
+  "A cross between group-by and walk
+
+by: the grouping function
+inner: transform on each collected object
+outer: run on each grouped collection"
+
+  [by inner outer xs]
+  (letfn [(reducer [acc o]
+            (update-in acc [(by o)] conj (inner o)))
+
+          (walker [[k v]]
+            [k (outer v)])]
+
+    (->> (reduce reducer {} xs)
+         (walk walker identity))))
--- test/clojure/swissknife/core_test.clj
+++ test/clojure/swissknife/core_test.clj
@@ -9,3 +9,9 @@
 
  (keep-last 3 [0 1])
  => [0 1])
+
+
+(facts
+ (group even? #(* % %) (partial apply +) (range 10))
+ => {false 165                          ; [1 9 25 49 81]
+     true 120})                         ; [0 4 16 36 64]
