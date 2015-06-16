aa412465ee43ed8581edac0fdb3da3e106a6574d
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2015-06-16 13:10:46 +0530

    indentation

--- src/clojure/swissknife/core.clj
+++ src/clojure/swissknife/core.clj
@@ -10,12 +10,12 @@
 
 (defn group
   "A cross between group-by and walk, applies
-inner and outer to the grouped collections instead
-of the entire map
+  inner and outer to the grouped collections instead
+  of the entire map
 
-by: the grouping function
-inner: transform on each collected object
-outer: run on each grouped collection"
+  by: the grouping function
+  inner: transform on each collected object
+  outer: run on each grouped collection"
 
   [by inner outer xs]
   (letfn [(reducer [acc o]
@@ -31,35 +31,35 @@ outer: run on each grouped collection"
 (defn bucket
   "Partitions a collection into fractions of a total, keeps order
 
-by: transforms object to number
-n: number of buckets
-total: the whole which a fraction is compared to"
+  by: transforms object to number
+  n: number of buckets
+  total: the whole which a fraction is compared to"
 
   ([by xs]
-     (bucket by 10 xs))
+   (bucket by 10 xs))
   ([by n xs]
-     (bucket by n (reduce + (map by xs)) xs))
+   (bucket by n (reduce + (map by xs)) xs))
   ([by n total xs]
-     (letfn [(take-fraction [coll]
-               (loop [acc [] sum 0 c coll]
-                 (cond (empty? c)
-                       acc
+   (letfn [(take-fraction [coll]
+             (loop [acc [] sum 0 c coll]
+               (cond (empty? c)
+                     acc
 
-                       (< sum (/ total n))
-                       (recur (conj acc (first c)) (+ sum (by (first c))) (rest c))
+                     (< sum (/ total n))
+                     (recur (conj acc (first c)) (+ sum (by (first c))) (rest c))
 
-                       :else
-                       acc)))]
-       (lazy-seq
-        (if (and (pos? total) (pos? n))
-          (let [f (take-fraction xs)]
-            (cons f (bucket by (dec n) (- total (/ total n)) (nthrest xs (count f))))))))))
+                     :else
+                     acc)))]
+     (lazy-seq
+      (if (and (pos? total) (pos? n))
+        (let [f (take-fraction xs)]
+          (cons f (bucket by (dec n) (- total (/ total n)) (nthrest xs (count f))))))))))
 
 
 (defn map-keys
   "Returns a new map with keys transformed by f
 
-If 2 or more keys are transformed to the same result, only one of the values is retained"
+  If 2 or more keys are transformed to the same result, only one of the values is retained"
   [f m]
   (letfn [(walker [[k v]]
             [(f k) v])]
