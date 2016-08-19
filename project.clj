337e09dbdda7d3dc1450fa2a5c3142e922ca4388
Sidhant Godiwala <sidhant.godiwala@booking.com>
2016-08-19 11:24:16 +0200

    bump clojure version

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "general purpose utility functions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "0.0-2173"]]
   :source-paths ["src/clojure"]
   :test-paths ["test/clojure"]
@@ -15,6 +15,4 @@
               :test-commands {"test" ["phantomjs"
                                       "test/cljs/tests.js"
                                       "test/cljs/tests.html"]}}
-  :profiles {:dev {:plugins [[lein-midje "3.1.3"]
-                             [lein-cljsbuild "1.0.2"]]
-                   :dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
