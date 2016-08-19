d5090cebd6d69add5812c28c95672bf787219e30
Sidhant Godiwala <sidhant.godiwala@booking.com>
2016-08-19 11:26:24 +0200

    version 1.0.0
    remove cljs (very out of date)

--- project.clj
+++ project.clj
@@ -1,18 +1,6 @@
-(defproject swissknife "0.9.2"
+(defproject swissknife "1.0.0"
   :description "general purpose utility functions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "0.0-2173"]]
-  :source-paths ["src/clojure"]
-  :test-paths ["test/clojure"]
-  :cljsbuild {:builds [{:id "simple"
-                        :source-paths ["test/cljs" "src/cljs"]
-                        :compiler {:optimizations :simple
-                                   :pretty-print true
-                                   :static-fns true
-                                   :output-to "test/cljs/tests.js"}}]
-              :test-commands {"test" ["phantomjs"
-                                      "test/cljs/tests.js"
-                                      "test/cljs/tests.html"]}}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
