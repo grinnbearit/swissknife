60acdfd2d2197421b1e8a2d4649964b5907abd96
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2018-02-24 16:26:56 +0100

    bump the clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject swissknife "1.0.0"
+(defproject swissknife "1.1.0"
   :description "general purpose utility functions"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[midje "1.9.0"]]}})
